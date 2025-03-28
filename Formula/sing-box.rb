67c64887aacadde60a4a95edd595f9cb0d42347a
imhansai <imhansai@foxmail.com>
2025-03-28 09:27:55 +0800

    sing-box 1.11.6

--- Formula/sing-box.rb
+++ Formula/sing-box.rb
@@ -2,8 +2,8 @@ class SingBox < Formula
   desc "Universal proxy platform"
   homepage "https://sing-box.sagernet.org"
   # using `:homebrew_curl` to work around audit failure from TLS 1.3-only homepage
-  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.11.4.tar.gz", using: :homebrew_curl
-  sha256 "633e40e2a64937069ada9d8d96dec1a5d735095d44eb3cbea105ee05f4616fc6"
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.11.6.tar.gz", using: :homebrew_curl
+  sha256 "01b697683c2433d93e3a15a362241e5e709c01a8e4cc97c889f6d2c5f9db275e"
   license "GPL-3.0-or-later"
 
   depends_on "go" => :build
